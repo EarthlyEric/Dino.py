5834c90672d26f915db8f7daa2a34bff5bb17f18
EarthlyEric6 <minecaeft.creeper.eric@gmail.com>
2022-07-20 18:55:53 +0800

    add basic pygame code

---
+++ main.py
@@ -0,0 +1,27 @@
+# Dino.py 
+# Made by EarthlyEric6
+import sys
+import pygame
+from pygame.locals import QUIT
+import object
+
+# PyGame Init
+pygame.init()
+
+width, height = 640, 360
+screen = pygame.display.set_mode((width, height))   
+pygame.display.set_caption("Dino.py")
+
+GameClock = pygame.time.Clock() 
+
+# Game Event Loop
+while True:
+    # Update Game Screen 60 times per second.
+    GameClock.tick(60) 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update() 
+   
