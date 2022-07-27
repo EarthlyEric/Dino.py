ee1490a6c4e50e84e6a2af89a0019c48aed6e79b
EarthlyEric6 <minecaeft.creeper.eric@gmail.com>
2022-07-27 22:02:27 +0800

    update game animation

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
 # Dino.py 
 # Made by EarthlyEric6
 import sys
 import os
 import pygame
+import game_animation
 from pygame.locals import QUIT
 import object
 
 # PyGame Init
 pygame.init()
 
-width, height = 640, 360
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_icon(pygame.image.load(os.path.join("dino.png")))
-pygame.display.set_caption("Dino.py")
+# Import Game Resource
+background_image = pygame.image.load(os.path.join('resource/background/horizon.png'))
 
-GameClock = pygame.time.Clock() 
+# Setup Game Settings
+width, height = 1280,720
+game_animation_speed = 20
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_icon(pygame.image.load(os.path.join('dino.png')))
+pygame.display.set_caption('Dino.py')
+screen.fill((255,255,255))
+GameClock = pygame.time.Clock()
+
 
 # Game Event Loop
 while True:
@@ -24,5 +33,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.update() 
+    game_animation.background_animation()
+    pygame.display.update()
    
--- object.py
+++ object.py
@@ -4,6 +4,5 @@ import os
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join())
+        self.image = pygame.image.load(os.path.join(""))
         self.rect = self.image.get_rect()
-
